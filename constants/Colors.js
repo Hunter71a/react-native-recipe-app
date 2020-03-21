c1ed1090c3e44136b57387ce2fcb8750d729ea6e
Carl Olson <43393940+Hunter71a@users.noreply.github.com>
2020-03-20 20:33:04 -0400

    add styling to fonts for ios and android

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,4 +1,6 @@
 export default {
   primaryColor: '#4a148c',
-  accentColor: '#ff6f00'
+  accentColor: '#ff6f00',
+  blueColor:  '#2655ff',
+  aquaColor: '#50adfa'
 };
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -19,6 +19,12 @@ import MealDetailScreen from '../screens/MealDetailScreen';
 const defaultStackNavOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans'
   },
   headerTintColor:
     Platform.OS === 'android' ? 'white' : Colors.primaryColor,
@@ -63,7 +69,8 @@ const tabScreenConfig = {
       tabBarIcon: (tabInfo) => {
         return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />;
       },
-      tabBarColor: Colors.primaryColor
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Meals</Text> : 'Meals'
     }
   },
   Favorites: {
@@ -73,7 +80,9 @@ const tabScreenConfig = {
       tabBarLabel: 'Favorites!',
       tabBarIcon: (tabInfo) => {
         return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
-      }
+      },
+      tabBarColor: Colors.accentColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{ fontFamily: 'open-sans-bold' }}>Favorites</Text> : 'Favorites'
     }
   }
 };
@@ -107,13 +116,23 @@ const MealsFavTabNavigator =
       //   }
       // }, {
       tabBarOptions: {
+        labelStyle: {
+          fontFamily: 'open-sans-bold'
+        },
         activeTintColor: Colors.accentColor
       }
     });
 
-const FiltersNavigator = createStackNavigator({
-  Filters: FiltersScreen
-});
+const FiltersNavigator = createStackNavigator(
+  {
+    Filters: FiltersScreen
+  },
+  {
+    navigationOptions: {
+      drawerLabel: "Filters"
+    },
+    defaultNavigationOptions: defaultStackNavOptions
+  });
 
 const MainNavigator = createDrawerNavigator({
   MealFavs: {
@@ -128,7 +147,8 @@ const MainNavigator = createDrawerNavigator({
     contentOptions: {
       activeTintColor: Colors.accentColor,
       labelStyle: {
-        fontFamily: 'open-sans-bold'
+        fontFamily: 'open-sans-bold', 
+       // textAlign: ''
       }
     }
   });
